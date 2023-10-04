9b9ecac1b40f973b5d6114cb80a178baa2c964ef
landenf <landenfogle@gmail.com>
2023-10-04 13:28:38 -0700

    Pin Location Update on Click

--- React/SwarmPlanner/src/components/MapContainer.js
+++ React/SwarmPlanner/src/components/MapContainer.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
-import droneIcon from '../resources/images/Simple_drone_small.svg'
+import droneIcon from '../resources/images/Simple_drone_small.svg';
+import pinIcon from '../resources/images/DroppedPin.svg'
 
 class MapContainer extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            center: { lat: 40.84667, lng: -96.471667}
+            center: { lat: 40.84667, lng: -96.471667}, pin : {lat: 40.847, lng: -96.471667}
         }
+    }
+
+    updatePinLocation = (newLat, newLng) => {
+      this.setState({
+        pin: { lat: newLat, lng: newLng}
+      });
     }
 
   render() {
@@ -25,7 +32,10 @@ class MapContainer extends React.Component {
         )
       })
 
-      const debug = <MarkerF label={'2'} icon={droneIcon} position={ {lat: 40.847, lng: -96.471667} }/>
+      const debugDrone = <MarkerF label={'2'} icon={droneIcon} position={ {lat: 40.847, lng: -96.471667} }/>
+      const {lat, lng} = this.state.pin;
+      const markerPin = <MarkerF label={'2'} icon={pinIcon} position={{lat: lat, lng: lng}}/>
+
 
     return (
         <LoadScript
@@ -39,11 +49,13 @@ class MapContainer extends React.Component {
                       fetch('http://127.0.0.1:8080/debug_vector/C1$9/' + (ev.latLng.lat() + '/') + (ev.latLng.lng() + '/') + "0" );
                             console.log("latitide = ", ev.latLng.lat());
                             console.log("longitude = ", ev.latLng.lng());
-                            this.setState({ center: {lat: ev.latlng.lat() ,lng:ev.latlng.lng() } })
-                            fetch('http://127.0.0.1:8080/debug_vector/C1$4/' + '')
+                            this.updatePinLocation(ev.latLng.lat(), ev.latLng.lng());
+                            fetch('http://127.0.0.1:8080/debug_vector/C1$4/' + '');
                         }}
                     >
             {agentMarkers}
+            {markerPin}
+            {debugDrone}
             </GoogleMap>
         </LoadScript>
     )
