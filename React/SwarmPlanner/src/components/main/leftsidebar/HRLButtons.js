898d3eda2888db32cfd29ee4f24c20a0f0ba4025
gus <grantphllps@gmail.com>
2024-02-26 22:51:59 -0600

    got rid of teh gpu thing for reasons

--- React/SwarmPlanner/src/components/main/leftsidebar/HRLButtons.js
+++ React/SwarmPlanner/src/components/main/leftsidebar/HRLButtons.js
@@ -25,7 +25,7 @@ class HRLButtons extends React.Component {
         return(
             <div>
                 <div className="center-text">
-                    <p className="PanelLabel" >Verifier Control Actions</p>
+                    <p className="PanelLabel" >Swarm Control Actions</p>
                 </div>
                 {/* <div className="center-input">
                     <p className="PanelSubLabel">Target Group: </p>
@@ -35,16 +35,16 @@ class HRLButtons extends React.Component {
                     <DebugIntButton class="ArmButton" title="START ROS" value={"1"}/>
                     <DebugIntButton class="DisarmButton" title="STOP ROS" value={"2"}/>
                 </div>
-                {/* <div className="center">
-                    <DebugVectorButton class="ModeButton" title="GUIDED" clusterId={this.state.clusterId} clusterCommand="3" option1="1" />
-                    <DebugVectorButton class="ModeButton" title="ALT SEP" clusterId={this.state.clusterId} clusterCommand="3" option1="4" />
-                    <DebugVectorButton class="ModeButton" title="TRACK"   clusterId={this.state.clusterId} clusterCommand="3" option1="5" />
-                </div> */}
-                {/* <div className="center">
-                    <DebugVectorButton class="ModeButton" title="HRL NAV" clusterId={this.state.clusterId} clusterCommand="3" option1="9" />
-                    <DebugVectorButton class="ModeButton" title="RALLY" clusterId={this.state.clusterId} clusterCommand="3" option1="1" />
-                    <DebugVectorButton class="ModeButton" title="SEND IT"   clusterId={this.state.clusterId} clusterCommand="3" option1="5" />
-                </div> */}
+                <div className="center">
+                    <DebugVectorButton class="ModeButton" title="GUIDED" clusterId={1} clusterCommand="3" option1="1" />
+                    <DebugVectorButton class="ModeButton" title="ALT SEP" clusterId={1} clusterCommand="3" option1="4" />
+                    <DebugVectorButton class="ModeButton" title="TRACK"   clusterId={1} clusterCommand="3" option1="5" />
+                </div>
+                <div className="center">
+                    <DebugVectorButton class="ModeButton" title="HRL NAV" clusterId={1} clusterCommand="3" option1="9" />
+                    <DebugVectorButton class="ModeButton" title="RALLY" clusterId={1} clusterCommand="3" option1="1" />
+                    <DebugVectorButton class="ModeButton" title="SEND IT"   clusterId={1} clusterCommand="3" option1="5" />
+                </div>
                 {/* <div className="center">
                     <DebugVectorButton class="OrangeButton" title="ENABLE ORBIT" clusterId={this.state.clusterId} clusterCommand="11"/>
                     <DebugVectorButton class="OrangeButton" title="DISABLE ORBIT" clusterId={this.state.clusterId} clusterCommand="12"/>
@@ -58,12 +58,12 @@ class HRLButtons extends React.Component {
                     </div>
                     <ThreeValueInput buttonFcn={this.waypointSend}/>
                 </div> */}
-                <div className="HRLSeparate">
+                {/* <div className="HRLSeparate">
                     <div className="AgentStatusCardSection-Regular">
                         <p className="PanelSubLabel"> Send Collision Point (LAT, LON, ALT)</p>
                     </div>
                     <ThreeValueInput buttonFcn={this.goTo}/>
-                </div>
+                </div> */}
                 {/* <div className="HRLSeparate">
                     <div className="AgentStatusCardSection-Regular">
                         <p className="PanelSubLabel">SET TARGET SYSTEM</p>
